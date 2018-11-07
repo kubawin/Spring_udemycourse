097c8c5a2c43924a907c2f5dca827b6dcd6f85f4
kuba <bubi78@gmail.com>
2018-11-07 16:33:50 +0100

    move on

---
+++ spring-demo-annotations/src/main/java/com/luv2code/springdemo/DatabaseFortuneService.java
@@ -0,0 +1,9 @@
+package com.luv2code.springdemo;
+
+public class DatabaseFortuneService implements IFortuneService {
+
+	public String getFortune() {
+		return "DatabaseFortune";
+	}
+
+}
---
+++ spring-demo-annotations/src/main/java/com/luv2code/springdemo/RESTFortuneService.java
@@ -0,0 +1,10 @@
+package com.luv2code.springdemo;
+
+public class RESTFortuneService implements IFortuneService {
+
+	public String getFortune() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
